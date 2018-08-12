7c660cfa3c7c37e8b9892dd67b4d8992dff6fe04
Ricardo Miranda <ricardomirannda@gmail.com>
2018-08-12 12:39:50 -0300

    Implementacao da funcao que realiza o filtro por usuario

--- filtrar.js
+++ filtrar.js
@@ -3,28 +3,27 @@ let campoFiltro = document.querySelector("#filtrar-tabela");
 campoFiltro.addEventListener("input", function () {
   let pacientesTodos = document.querySelectorAll(".paciente");
 
+  // this.value é o campo filtro que está no HTML
   if ( this.value.length > 0 ){
     pacientesTodos.forEach( paciente => {
 
       let tdNome = paciente.querySelector(".info-nome");
       let nome = tdNome.textContent;
 
-      let expressao = new RegExp( this.value, "insenstive");
-      
+      let expressao = new RegExp( this.value, "i");
 
-
-
-      // this.value é o campo filtro que está no HTML
-      if ( nome == this.value ){
-        paciente.classList.remove("invisivel");
-      } else {
+      if ( !expressao.test(nome) ){
         paciente.classList.add("invisivel");
+      } else {
+        paciente.classList.remove("invisivel");
+        paciente.classList.add("selecionado")
       }
     })
 
   } else {
     pacientesTodos.forEach( paciente => {
       paciente.classList.remove("invisivel");
+      paciente.classList.remove("selecionado")
     })
   }
 
